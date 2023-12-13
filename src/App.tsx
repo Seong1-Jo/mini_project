eead861547d78c0a72af3c004ea50eefc26f4489
Seong1-Jo <seong12yada@gmail.com>
2023-12-13 21:13:06 +0900

    Date: 2023-12-13

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <h1>시술예약</h1>
+            
+      <div className="firstInputContainer">
+                <span>이름 : </span> <input type="text" /> <br />
+                <span>아이디 : </span> <input type="text" />
+              
+      </div>
+            
+      <div className="secondInputContainer">
+        시술         
+        <ul>
+                    
+          <li>
+                        
+            <input type="checkbox" />
+                        <span>왁싱</span>
+                      
+          </li>
+                    
+          <li>
+                        
+            <input type="checkbox" />
+                        <span>네일</span>
+                      
+          </li>
+                    
+          <li>
+                        
+            <input type="checkbox" />
+                        <span>피부관리</span>
+                      
+          </li>
+                    
+          <li>
+                        
+            <input type="checkbox" />
+                        <span>기타</span>
+                      
+          </li>
+                  
+        </ul>
+              
+      </div>
+            
+      <div>
+                <span>개인정보 제공동의</span>
+                
+        <input type="checkbox" />
+                <button>제출</button>
+              
+      </div>
     </div>
   );
 }
